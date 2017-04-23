8bc9bfad129f271e7d0f912e52e4f55fe0744c1e
argraur <argraur@gmail.com>
2017-04-23 08:22:28 +0000

    Various vendor build fixes
    Change-Id: I941bfc546d5fee7431f7eb7f478a16f6a1bee7af

--- angler.mk
+++ angler.mk
@@ -10,6 +10,11 @@ $(call inherit-product, device/sample/products/backup_overlay.mk)
 # Inherit Open GApps configuration
 $(call inherit-product, device/huawei/angler/opengapps.mk)
 
+# Build vendor from source if needed
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),ext4)
+$(call inherit-product, vendor/huawei/angler/vendorimage.mk)
+endif
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := angler
 PRODUCT_BRAND := google
@@ -17,7 +22,10 @@ PRODUCT_MODEL := Pixel XL
 TARGET_MANUFACTURER := Huawei
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Only if vendor build disabled
+ifneq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),ext4)
 BUILD_FINGERPRINT := google/angler/angler:7.1.2/N2G47H/3783593:user/release-keys
+endif
 
 PRODUCT_PACKAGES += \
 	AOSPLinks
